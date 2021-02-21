02e60cd3ce13858ad47592e34a808fa426beacfe
SihunHwang <74863441+SihunHwang@users.noreply.github.com>
2021-02-22 01:21:34 +0900

    Add files via upload

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
-# CS50_AI is Massive Online Open Course.
-
-There are 7 chapeters.
-
-0. Search
-1. Knowledge
-2. Uncertainty
-3. Optimization
-4. Learning
-5. Neural Networks
-6. Language
-
-Each chapter is aligned with 2 projects.
-You can see my work on each project on the corresponding branch of this repository.
+# SihunHwang
---
+++ parser.py
@@ -0,0 +1,103 @@
+import nltk
+import sys
+
+TERMINALS = """
+Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
+Adv -> "down" | "here" | "never"
+Conj -> "and" | "until"
+Det -> "a" | "an" | "his" | "my" | "the"
+N -> "armchair" | "companion" | "day" | "door" | "hand" | "he" | "himself"
+N -> "holmes" | "home" | "i" | "mess" | "paint" | "palm" | "pipe" | "she"
+N -> "smile" | "thursday" | "walk" | "we" | "word"
+P -> "at" | "before" | "in" | "of" | "on" | "to"
+V -> "arrived" | "came" | "chuckled" | "had" | "lit" | "said" | "sat"
+V -> "smiled" | "tell" | "were"
+"""
+
+NONTERMINALS = """
+S -> NP VP | S Conj S | S Conj VP | PP S 
+NP -> N | Adj NP | NP PP | Det NP
+PP -> P NP | P S
+VP -> V | VP NP | Adv VP | VP Adv | VP PP
+"""
+
+grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
+parser = nltk.ChartParser(grammar)
+
+
+def main():
+
+    # If filename specified, read sentence from file
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            s = f.read()
+
+    # Otherwise, get sentence as input
+    else:
+        s = input("Sentence: ")
+
+    # Convert input into list of words
+    s = preprocess(s)
+
+    # Attempt to parse sentence
+    try:
+        trees = list(parser.parse(s))
+    except ValueError as e:
+        print(e)
+        return
+    if not trees:
+        print("Could not parse sentence.")
+        return
+
+    # Print each tree with noun phrase chunks
+    for tree in trees:
+        tree.pretty_print()
+
+        print("Noun Phrase Chunks")
+        for np in np_chunk(tree):
+            print(" ".join(np.flatten()))
+
+
+def preprocess(sentence):
+    """
+    Convert `sentence` to a list of its words.
+    Pre-process sentence by converting all characters to lowercase
+    and removing any word that does not contain at least one alphabetic
+    character.
+    """
+    s = nltk.word_tokenize(sentence)
+    r = []
+    for w in s:
+        for l in w:
+            b = True
+            if l.isalpha():
+                break
+            b = False
+        if b:
+            r.append(w.lower())
+    return r
+
+
+def np_chunk(tree):
+    """
+    Return a list of all noun phrase chunks in the sentence tree.
+    A noun phrase chunk is defined as any subtree of the sentence
+    whose label is "NP" that does not itself contain any other
+    noun phrases as subtrees.
+    """
+    npc = []
+    
+    for np in tree.subtrees(lambda t: t.label() == 'NP'):
+        for sub in np.subtrees(lambda t: t != np):
+            b = False
+            if sub.label() == 'NP':
+                break
+            b = True
+        if b:
+            npc.append(np)
+    
+    return npc
+
+
+if __name__ == "__main__":
+    main()
